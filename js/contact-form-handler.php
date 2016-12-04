22a66ecd07d60ab4088691ab38e78fe8c284a308
alexolivet <alexolivet@yahoo.com>
2016-12-04 20:08:08 +0100

    email text changed to address scope of project

--- js/contact-form-handler.php
+++ js/contact-form-handler.php
@@ -9,16 +9,16 @@ if(empty($_POST['message']))
 $name = $_POST['name']; 
 $email_address = $_POST['email']; 
 $message = $_POST['message'];
-$age=$_POST['message_1']; 
+$stickers=$_POST['message_1']; 
 
 if( empty($errors))
 {
 	$to = $email_address;
 	$email_subject = "Thank you : $name for contacting Stickers by elwebman.nl";
-	$email_body = "Your positive words have arrived!. ".	// this is the content of the email
+	$email_body = "A few words on how we operate. ".	// this is the content of the email
 	" <html>
 	<head>
-	<title>Positive Word generator</title>
+	<title>elwebman.nl stickers</title>
 	<style>
 	p{
 		font-size=20px;
@@ -40,21 +40,22 @@ if( empty($errors))
 	<p>Thank you for contacting us.</p>
 	<br>
 	<br>
-	<p>Your Positive Words Have Arrived!</p>
-	<p>We hope that they will help you and motivate you!</p>
+	<p>We have taken your interest into account.</p>
+	<p>We are preparing the stickers.</p>
 	<br>
 	<br>
-	<p>Your Positive Words for this time are...</p>
+	<p>You have made  the following comment on our website</p>
 	<br>
 	<h1>\n $message</h1>
 	<p>You have expressed interest in the following sticker:</p>
-	<h1>\n $age</h1>
+	<h1>\n $stickers</h1>
+	<p>Delivery will take more or less 10 days depending on where you are located.</p>
 	<br>
 	<br>
 	<br>
-	<p> The <a href=\"http://www.elwebman.nl/pwg/\">Positive Word Generator </a> is available 24 hours a day</p>
+	<p> <a href=\"http://www.stickers.elwebman.nl/pwg/\">Stickers by elwebman.nl </a> is available 24 hours a day</p>
 	<p>If your email address is not \n $email_address, then please delete this email</p>
-	<p>Thank you for contacting The Positive Word Generator</p>
+	<p>Thank you for contacting stickers by elwebman.nl</p>
 	<p>Check us on <a href=\"https://github.com/alexolivet/\">Github</a>.</p>   
 	</body>
 	</html>
@@ -76,7 +77,7 @@ if( empty($errors))
 <head>
 	<title>Contact form handler</title>
 	<!-- main css style sheet-->
-	<link rel="stylesheet" href="http://positivegenerator.elwebman.nl/css/style.css">
+	<link rel="stylesheet" href="http://stickers.elwebman.nl/css/main_style.css">
 	 <!-- font awesome declaration-->
   <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'>
 </head>
@@ -89,13 +90,13 @@ if( empty($errors))
 				echo nl2br($errors);
 				?>
 				<p>Generate your words and use the form to send them to your email address.</p>
-				<p> Go back to  <a href="http://positivegenerator.elwebman.nl/">Positive Word Generator </a>.</p>
+				<p> Go back to  <a href="http://stickers.elwebman.nl/">Stickers by elwebman.nl</a>.</p>
 				<div class="footer-icons">
 					<a href="https://www.facebook.com/elwebmanamsterdam/"><i class="fa fa-facebook"></i></a>
 					<a href="https://www.linkedin.com/in/alexolivet"><i class="fa fa-linkedin"></i></a>
 					<a href="https://github.com/alexolivet/final_project"><i class="fa fa-github"></i></a>
 				</div>
-				<p class="footer-company-name">Positive Words Generator &copy; 2016</p>
+				<p class="footer-company-name">elwebman.nl &copy; 2017</p>
 			</div>
 			<div class="cover-holder">
 				<img src="http://positivegenerator.elwebman.nl/images/sun.png" alt="positive word generator">
